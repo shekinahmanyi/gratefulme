db28c7340b79bc969f44e017304e997c1deb8ce7
shekinahmanyi2 <shekinah.manyi@neero.io>
2023-11-03 11:33:04 +0100

    Playing with items in the navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+
+
+  return (
+    
+    <container class="flex font-mono">
+      <div>GratefulMe</div>
+      <div>Home</div>
+      <div>About</div>
+      <div>Contact</div>
+    </container>
+  )
+}
+
+export default Navbar
