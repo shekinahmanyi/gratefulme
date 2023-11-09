5495fb9bb470f273326e3681626472e43e1eef67
shekinahmanyi <shekinahmanyi@gmail.com>
2023-11-09 14:32:47 +0100

    Added reducer counter

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from "react";
 //import Navbar from "./components/Navbar";
 import store from "./app/store";
 import { Provider } from "react-redux";
+import Counter from './features/counter/Counter'
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">{/* <Navbar/> */}</div>
+      <div className="App">
+        <Counter/>
+        {/* <Navbar/> */}
+        </div>
     </Provider>
   );
 }
