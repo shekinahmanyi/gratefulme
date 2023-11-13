401ee987b4671ca8e43399a072a8ffc9a60e95bf
shekinahmanyi <shekinahmanyi@gmail.com>
2023-11-13 14:53:53 +0100

    installed router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
-//import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import store from "./app/store";
 import { Provider } from "react-redux";
-import Counter from './features/counter/Counter'
+//import Counter from './features/counter/Counter'
 
 function App() {
   return (
     <Provider store={store}>
+      <Router>
       <div className="App">
-        <Counter/>
-        {/* <Navbar/> */}
+     {/*  <Counter/>*/}  
+       <Navbar/> 
         </div>
+      </Router>
+     
     </Provider>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
+  const toggleTheme = () => {
+    setIsDarkMode((prevMode) => !prevMode);
+  };
 
   return (
-    
-    <container class="flex font-mono">
-      <div>GratefulMe</div>
-      <div>Home</div>
-      <div>About</div>
-      <div>Contact</div>
-    </container>
-  )
-}
+    <nav
+      className={`navbar ${
+        isDarkMode ? "dark:bg-slate-900 text-white" : "bg-white "
+      } fixed top-0 inset-x-0 z-40 shadow-lg`}
+    >
+      <div className="container mx-auto px-6 flex items-center font-nunito justify-between">
+        <div className="px-5 text-2xl mb-5 mt-5 font-bold">
+          <span className="">
+            GrateFul<span className="">Me</span>
+          </span>
+        </div>
 
-export default Navbar
+        <div className="font-bold hidden md:flex md:items-center md:space-x-4 mr-52">
+          <Link to="/">Home</Link>
+        </div>
 
+        <div className="flex items-center space-x-4">
+          <button onClick={toggleTheme}>
+            {isDarkMode ? "Light Mode" : "Dark Mode"}
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
+export default Navbar;
