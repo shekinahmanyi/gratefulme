237a73b19688679ccfe98ac4011003c4ab3fd060
shekinahmanyi <shekinahmanyi@gmail.com>
2023-11-13 16:16:49 +0100

    Added the isdarkmode

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import store from "./app/store";
 import { Provider } from "react-redux";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode((prevMode) => !prevMode);
+  };
+
   return (
     <Provider store={store}>
       <Router>
-        <Navbar />
+        <Navbar isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home isDarkMode={isDarkMode} />} />
         </Routes>
         <Footer />
       </Router>
