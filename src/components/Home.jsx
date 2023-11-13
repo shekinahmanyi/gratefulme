56ea781ce3b455b5a600e1864bf0f7d41a77f1f1
shekinahmanyi <shekinahmanyi@gmail.com>
2023-11-13 16:17:22 +0100

    Added contents for the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from "react";
 
-const Home = () => {
+const Home = ({ isDarkMode }) => {
   return (
-    <div>
-         <h1>
-      This is Home
-    </h1>
+    <div
+      className={`home-container ${
+        isDarkMode ? "dark:bg-slate-900 text-white" : "bg-white text-black"
+      } mt-24`}
+    >
+      <h1>Welcome to GrateFulMe!</h1>
+      <p>This is your home page content.</p>
     </div>
-   
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
