9441be48f14ef6885d79f99a999a86c41b4ee594
shekinahmanyi <shekinahmanyi@gmail.com>
2023-11-13 18:53:27 +0100

    updated fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      backgroundColor: {
+        "custom-blue": "#f1f5f9",
+      },
       fontFamily: {
-        
-      }
+        custom: [
+          "var(--font-sans)",
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          '"Segoe UI"',
+          "Roboto",
+          '"Helvetica Neue"',
+          "Arial",
+          '"Noto Sans"',
+          "sans-serif",
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+      },
     },
   },
   plugins: [],
-}
-
+};
