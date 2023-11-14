01a936baffaafafe79e41e6728a5c544aba1dcd6
shekinahmanyi <shekinahmanyi@gmail.com>
2023-11-14 13:30:24 +0100

    made changes for the dark theme

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,22 +6,29 @@ const Home = ({ isDarkMode }) => {
   return (
     <div
       className={`home-container ${
-        isDarkMode ? "dark:bg-slate-900 text-white dark:text-slate-400" : "bg-custom-blue text-black min-h-screen"
-      } text-center font-custom pt-32`}
+        isDarkMode
+          ? "dark:bg-slate-900 text-white "
+          : "bg-custom-blue text-black"
+      } text-center font-custom pt-24 h-screen`}
     >
-      <h1 className="text-5xl font-extrabold leading-tight tracking-tighter sm:text:4xl md:text-5xl lg:text-6xl">
+      <h1 className="text-5xl font-extrabold leading-tight tracking-tighter sm:text-5xl md:text-5xl lg:text-6xl">
         Stay grateful in <br />
         your future endeavors
       </h1>
-      <p className="text-slate-700  text-base sm:text-sm md:text-xl text-center mt-4">
-        <span className="text-center">
-        What are you grateful for at the moment ? Send a message to the <br />
+      <p className="text-slate-700 dark:text-slate-400  text-base sm:text-sm md:text-xl text-center mt-4">
+        <span className="text-center hidden sm:inline">
+          What are you grateful for at the moment ? Send a message to the <br />
+          future, so you can watch ,
         </span>
-       future, so you can watch back when you feel tired and overwhelmed and<br /> be grateful to God for how
-         far you've come.
+        when you feel tired and overwhelmed and
+        <br /> be grateful to God for how far you've come.
       </p>
-      <button className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 bg-slate-900 text-white text-base font-medium px-4 py-2 rounded-md mt-6">Record a Message</button>
-      <button className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 bg-white text-base font-medium px-4 py-2 rounded-md mt-6 ml-6">Learn How it's built</button>
+      <button className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 bg-black text-white text-base font-medium px-4 py-2 rounded-md mt-12">
+        Record a Message
+      </button>
+      <button className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 bg-white text-base font-medium px-4 py-2 rounded-md mt-12 ml-6">
+        Learn How it's built
+      </button>
     </div>
   );
 };
