c0cd4057a56a2541df4330437288dd27cfaca5ba
shekinahmanyi <shekinahmanyi@gmail.com>
2023-12-07 18:59:07 +0100

    Refactor ImageGallery component

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,44 +1,33 @@
-
-import React from 'react';
+import React from "react";
 import sheki from "../images/sheki.jpeg";
-import { Transition } from '@headlessui/react';
+//import { Transition } from "@headlessui/react";
 
 const ImageGallery = () => {
-    const showImage = true;
+    // const showImage = true;
 
     return (
-        <div className="flex w-80 h-90 mt-16  mx-auto my-auto text-center sm:flex-row sm:w-50 sm:h-50 sm:mt-0 sm:ml-0 sm:mr-0">
-            <Transition
-                show={showImage}
-                enter="transition-opacity duration-500"
-                enterFrom="opacity-0"
-                enterTo="opacity-100"
-                leave="transition-opacity duration-500"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-            >
+        <div className="flex flex-col sm:flex-row w-1/2 h-fit mt-32 text-center sm:w-1/2 sm:h-fit sm:mt-0 sm:ml-0 sm:mr-0">
+            <div className="ml-12 sm:mb-0">
                 <img
                     src={sheki}
                     alt="shekinah"
-                    className="rounded-lg object-cover w-full h-full scale-125 origin-top-left mr-32"
+                    className="rounded-xl object-cover w-full h-full scale-125 origin-top-left sm:ml-0 sm:mr-0"
                 />
-            </Transition>
-
-            <Transition
-                show={showImage}
-                enter="transition-opacity duration-500 delay-100"
-                enterFrom="opacity-0"
-                enterTo="opacity-100"
-                leave="transition-opacity duration-500"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-            >
+            </div>
+            <div className="ml-32 ">
                 <img
                     src={sheki}
                     alt="shekinah"
-                    className="rounded-lg object-cover w-full h-full scale-125 origin-top-left mr-32"
+                    className="rounded-xl object-cover w-full h-full scale-125 origin-top-left sm:ml-0 sm:mr-0"
                 />
-            </Transition>
+            </div>
+            <div className="ml-32 ">
+                <img
+                    src={sheki}
+                    alt="shekinah"
+                    className="rounded-xl object-cover w-full h-full scale-125 origin-top-left sm:ml-0 sm:mr-0"
+                />
+            </div>
         </div>
     );
 };
