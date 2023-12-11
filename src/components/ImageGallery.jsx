ac45d6dcebc10485334e5185b194a584506de129
shekinahmanyi <shekinahmanyi@gmail.com>
2023-12-11 16:41:21 +0100

    Refactor ImageGallery component and add parallax effect

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,24 +1,75 @@
-import React from "react";
-import sheki from "../images/sheki.jpeg";
-import { motion } from "framer-motion";
-
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useRef } from "react";
+import { motion, useAnimation } from "framer-motion";
+import h3 from '../images/h3.jpeg'
+import h4 from '../images/h4.jpeg'
+import h2 from '../images/h2.jpeg'
+import h5 from '../images/h5.jpeg'
+import h7 from '../images/h7.jpeg'
+import h8 from '../images/h8.jpeg'
+import h9 from '../images/h9.jpeg'
+import h1 from '../images/h1.jpeg'
+import h6 from '../images/h6.jpeg'
+import h10 from '../images/h10.jpeg'
+import h11 from '../images/h11.jpeg'
+import Cards from "./Cards";
 
 
 const ImageGallery = () => {
+  const containerRef = useRef(null);
+  const controls1 = useAnimation();
+  const controls2 = useAnimation();
+  const controls3 = useAnimation();
+  const controls4 = useAnimation();
+
+  const updateParallaxPosition = () => {
+    if (!containerRef.current) return;
+
+    const scrollY = window.scrollY;
+    const containerTop = containerRef.current.getBoundingClientRect().top;
+
+    controls1.start({ translateY: (containerTop - scrollY) * 0.4 });
+    controls2.start({ translateY: (containerTop - scrollY) * 0.4 });
+    controls3.start({ translateY: (containerTop - scrollY) * 0.4 });
+    controls4.start({ translateY: (containerTop - scrollY) * 0.4 });
+
     
+  };
+
+  useEffect(() => {
+    updateParallaxPosition();
+
+    const handleScroll = () => {
+      updateParallaxPosition();
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="h-[175vh] p-5 [&>*:nth-of-type(1)]:-top-[45%] [&>*:nth-of-type(2)]:-top-[95%] [&>*:nth-of-type(3)]:-top-[45%] [&>*:nth-of-type(4)]:-top-[75%] overflow-hidden bg-[rgb(45, 45, 45)] flex gap-[2vw] box-border bg-custom-blue text-center dark:bg-slate-900 ">
-      {/*Transition1 */}
+    <div>
+    <div className="md:mt-[10vh]">
+        <div className="h-[23vh] md:h-[40vh] flex flex-col justify-center bg-custom-blue  dark:bg-slate-900">
+         <h1 className="uppercase  dark:text-white z-1 md:mt-[20vh] md:pl-[10vw] text-center md:text-left text-2xl md:text-3xl  "> A One Year of Reflection</h1>
+        </div>
+    <div
+      className="h-[175vh] p-5 [&>*:nth-of-type(1)]:-top-[15%] [&>*:nth-of-type(2)]:-top-[35%] [&>*:nth-of-type(3)]:-top-[35%] [&>*:nth-of-type(4)]:-top-[45%] overflow-hidden bg-[rgb(45, 45, 45)] flex gap-[2vw] box-border bg-custom-blue text-center dark:bg-slate-900 "
+      ref={containerRef}
+    >
+        <div></div>
+      {/* Transition1 */}
       <motion.div
-        className="w-[25%] h-full flex flex-col gap-[2vw] min-w-[150px] lg:min-w-[250px] "
-        initial="hidden"
-        whileInView="visible"
-        transform="translateY(653.621px) translateZ(0px)"
+        className="w-[25%] relative h-full flex flex-col gap-[2vw] min-w-[150px] lg:min-w-[250px]"
+        animate={controls1}
       >
         {/* Image 1 */}
         <motion.div className="w-full h-full relative rounded-[1vw] overflow-hidden">
           <img
-            src={sheki}
+            src={h1}
             alt="shekinah"
             className="rounded-xl object-cover w-full h-full scale-125 origin-top-left sm:ml-0 sm:mr-0"
           />
@@ -26,7 +77,7 @@ const ImageGallery = () => {
         {/* Image 2 */}
         <motion.div className="w-full h-full relative rounded-[1vw] overflow-hidden">
           <img
-            src={sheki}
+            src={h2}
             alt="shekinah"
             className="rounded-xl object-cover w-full h-full scale-125 origin-top-left sm:ml-0 sm:mr-0 "
           />
@@ -34,23 +85,22 @@ const ImageGallery = () => {
         {/* Image 3 */}
         <motion.div className="w-full h-full relative rounded-[1vw] overflow-hidden">
           <img
-            src={sheki}
+            src={h3}
             alt="shekinah"
             className="rounded-xl object-cover w-full h-full scale-125 origin-top-left sm:ml-0 sm:mr-0 "
           />
         </motion.div>
       </motion.div>
 
-      {/*Transition2 */}
+      {/* Transition2 */}
       <motion.div
-        className="w-[25%]  h-full flex flex-col gap-[2vw] min-w-[150px] lg:min-w-[250px] transform: translateY(653.621px) translateZ(0px);"
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
+        className="w-[25%] relative h-full flex flex-col gap-[2vw] min-w-[150px] lg:min-w-[250px]"
+        animate={controls2}
       >
         {/* Image 1 */}
         <motion.div className="w-full h-full relative rounded-[1vw] overflow-hidden">
           <img
-            src={sheki}
+            src={h4}
             alt="shekinah"
             className="rounded-xl object-cover w-full h-full scale-125 origin-top-left sm:ml-0 sm:mr-0"
           />
@@ -58,7 +108,7 @@ const ImageGallery = () => {
         {/* Image 2 */}
         <motion.div className="w-full h-full relative rounded-[1vw] overflow-hidden">
           <img
-            src={sheki}
+            src={h5}
             alt="shekinah"
             className="rounded-xl object-cover w-full h-full scale-125 origin-top-left sm:ml-0 sm:mr-0"
           />
@@ -66,23 +116,22 @@ const ImageGallery = () => {
         {/* Image 3 */}
         <motion.div className="w-full h-full relative rounded-[1vw] overflow-hidden">
           <img
-            src={sheki}
+            src={h7}
             alt="shekinah"
             className="rounded-xl object-cover w-full h-full scale-125 origin-top-left sm:ml-0 sm:mr-0"
           />
         </motion.div>
       </motion.div>
 
-      {/*Transition3 */}
+      {/* Transition3 */}
       <motion.div
-        className="w-[25%]  h-full flex flex-col gap-[2vw] min-w-[150px] lg:min-w-[250px] transform translate-y-36 translate-z-0"
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
+        className="w-[25%] relative h-full flex flex-col gap-[2vw] min-w-[150px] lg:min-w-[250px]"
+        animate={controls3}
       >
         {/* Image 1 */}
         <motion.div className="w-full h-full relative rounded-[1vw] overflow-hidden">
           <img
-            src={sheki}
+            src={h6}
             alt="shekinah"
             className="rounded-xl object-cover w-full h-full scale-125 origin-top-left sm:ml-0 sm:mr-0"
           />
@@ -90,7 +139,7 @@ const ImageGallery = () => {
         {/* Image 2 */}
         <motion.div className="w-full h-full relative rounded-[1vw] overflow-hidden">
           <img
-            src={sheki}
+            src={h8}
             alt="shekinah"
             className="rounded-xl object-cover w-full h-full scale-125 origin-top-left sm:ml-0 sm:mr-0"
           />
@@ -98,45 +147,51 @@ const ImageGallery = () => {
         {/* Image 3 */}
         <motion.div className="w-full h-full relative rounded-[1vw] overflow-hidden">
           <img
-            src={sheki}
+            src={h9}
             alt="shekinah"
-            className="rounded-xl object-cover w-full h-full scale-125 origin-top-left sm:ml-0 sm:mr-0"
+            className="rounded-xl object-cover  w-full h-full scale-125 origin-top-left sm:ml-0 sm:mr-0"
           />
         </motion.div>
       </motion.div>
 
-      {/*Transition4 */}
+      {/* Transition4 */}
       <motion.div
-        className="w-[25%] h-full flex flex-col gap-[2vw] min-w-[150px] lg:min-w-[250px] transform translate-y-96 translate-z-0"
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
+        className="w-[25%] relative h-full flex flex-col gap-[2vw] min-w-[150px] lg:min-w-[250px]"
+        animate={controls4}
       >
         {/* Image 1 */}
-        <motion.div className="w-full h-full relative rounded-[1vw] overflow-hidden">
+        <motion.div className="w-full h-full  rounded-[1vw] overflow-hidden">
           <img
-            src={sheki}
+            src={h10}
             alt="shekinah"
-            className="rounded-xl object-cover w-full h-full scale-125 origin-top-left sm:ml-0 sm:mr-0"
+            className="rounded-xl object-cover  w-full h-full scale-125 origin-top-left sm:ml-0 sm:mr-0"
           />
         </motion.div>
         {/* Image 2 */}
-        <motion.div className="w-full h-full relative rounded-[1vw] overflow-hidden">
+        <motion.div className="w-full h-full  rounded-[1vw] overflow-hidden">
           <img
-            src={sheki}
+            src={h11}
             alt="shekinah"
-            className="rounded-xl object-cover w-full h-full scale-125 origin-top-left sm:ml-0 sm:mr-0"
+            className="rounded-xl object-cover  w-full h-full scale-125 origin-top-left sm:ml-0 sm:mr-0"
           />
         </motion.div>
         {/* Image 3 */}
-        <motion.div className="w-full h-full relative rounded-[1vw] overflow-hidden">
+        <motion.div className="w-full h-full rounded-[1vw] overflow-hidden top:-[10%]">
           <img
-            src={sheki}
+            src={h4}
             alt="shekinah"
-            className="rounded-xl object-cover w-full h-full scale-125 origin-top-left sm:ml-0 sm:mr-0"
+            className="rounded-xl object-cover  w-full h-full scale-125 origin-top-left sm:ml-0 sm:mr-0 "
+          
           />
         </motion.div>
+      
       </motion.div>
     </div>
+    
+    </div>
+    <Cards/>
+    </div>
+
   );
 };
 
