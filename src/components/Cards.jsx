be012993064721d368a9318de2230cd38cec1994
shekinahmanyi <shekinahmanyi@gmail.com>
2023-12-11 16:41:54 +0100

    Add Cards component

---
+++ src/components/Cards.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <div className="px-5 flex items-center justify-center pb-10 bg-custom-blue  dark:bg-slate-900">
+        <div data-scroll="true" data-scroll-speed="1.1" className="w-full md:w-[70%] max-w-[800px] bg-[#141319] p-5 md:p-8 rounded-[2vw] shadow-inner flex flex-col gap-5 text-gray-300 border border-gray-600 is-inview">
+
+        </div>
+    </div>
+  )
+}
+
+export default Cards
