12296ac1053b911af59cde4d0b172652f7c3ddbf
shekinahmanyi <shekinahmanyi@gmail.com>
2023-12-12 11:21:08 +0100

    Refactor Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
-const Footer = () => {
+
+function Footer() {
+  const footerYear = new Date().getFullYear();
+
   return (
-    <div>
-      
-    </div>
-  )
+    <footer className="relative p-10 text-primary-content flex flex-col justify-center items-center text-base dark:text-white  bg-custom-blue dark:bg-slate-900">
+      <div className="relative z-10 container ">     
+        <div className="text-center font-custom text-lg mt-12">
+          <p className="">Designed & Built with❤️by Sheki Dev✨</p>
+          <p className="">&copy; {footerYear} All rights reserved</p>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
