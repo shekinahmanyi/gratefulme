89a35390246ee3d96cf83133cf78d877968f82b7
shekinahmanyi <shekinahmanyi@gmail.com>
2024-01-02 15:40:14 +0100

    Refactor Footer component styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 
-
 function Footer() {
   const footerYear = new Date().getFullYear();
 
   return (
     <footer className="relative p-10 text-primary-content flex flex-col justify-center items-center text-base dark:text-white  bg-custom-blue dark:bg-slate-900">
-      <div className="relative z-10 container ">     
+      <div className="relative z-10 container ">
         <div className="text-center font-custom text-lg mt-12">
           <p className="">Designed & Built with❤️by Sheki Dev✨</p>
           <p className="">&copy; {footerYear} All rights reserved</p>
